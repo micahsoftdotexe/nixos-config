e0711cadd095c0d32afdf93fd92c5e02af118d9f
Micah Tanner <micahrocz777@gmail.com>
2023-06-19 20:27:14 -0400

    Updated script for updating system

--- generate-config.sh
+++ generate-config.sh
@@ -1,5 +1,6 @@
 if [ $1 ]; then
     if [ $1 = "update" ]; then
+        sudo nix-channel --update
         sudo nixos-rebuild switch -I nixos-config=./configuration/configuration.nix --upgrade
     fi
 else

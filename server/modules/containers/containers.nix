f467fbcf90a883dbd83121aedf7382928f7b7590
Micah Tanner <micahrocz777@gmail.com>
2023-09-26 01:04:07 +0000

    Updated home assistant

--- server/modules/containers/containers.nix
+++ server/modules/containers/containers.nix
@@ -5,7 +5,7 @@
 		containers.homeassistant = {
 			volumes = [ "/disk1/homeassistant/config:/config" ];
 			environment.TZ = "America/New_York";
-			image = "ghcr.io/home-assistant/home-assistant:2023.8.3"; # Warning: if the tag does not change, the image will not be updated
+			image = "ghcr.io/home-assistant/home-assistant:2023.9.3"; # Warning: if the tag does not change, the image will not be updated
 			extraOptions = [ 
 				"--network=host" 
 				"--device=/dev/ttyACM0:/dev/ttyACM0"  # Example, change this to match your own hardware

06a9638b85501f9140ac027a8f7eb6c2d7e9768b
Micah Tanner <micahrocz777@gmail.com>
2024-03-09 16:35:58 +0000

    Updated flake.lock

--- server/modules/containers/minecraft.nix
+++ server/modules/containers/minecraft.nix
@@ -1,20 +1,20 @@
 { config, pkgs, inputs, ... }: 
 {
-	virtualisation.oci-containers.containers.tardis = {
+	virtualisation.oci-containers.containers.whocraft = {
 		autoStart = true;
 		image = "itzg/minecraft-server";
 		ports = ["25565:25565"];
 		environment = {
 			EULA = "TRUE";
-			TYPE = "FORGE";
-			VERSION = "1.19.2";
-			INIT_MEMORY = "3G";
+			TYPE = "FABRIC";
+			VERSION = "1.20.1";
+			INIT_MEMORY = "4G";
 			MAX_MEMORY = "10G";
-			FORGE_VERSION = "43.3.0";
+			FABRIC_VERSION = "0.15.7";
 			REMOVE_OLD_MODS = "false";
 		};
 		volumes = [
-			"/disk1/minecraft/tardis:/data"
+			"/disk1/minecraft/whocraft:/data"
 		];
 	};
 }
